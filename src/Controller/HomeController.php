8d23c4d27aed0c75576267d2b7cc336635340553
monmedbox <auguste.monmedbox@gmail.com>
2023-06-24 16:52:57 +0100

    modif

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,6 +13,7 @@ class HomeController extends AbstractController
     {
 
         dd("ppppppppppp");
+        $a=1;
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
